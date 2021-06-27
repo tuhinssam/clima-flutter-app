a5b340135e7bd30e405ff52e22651a80fa45d2fa
tuhinssam <tuhinssam@gmail.com>
2021-06-27 16:17:22 +0530

    spinner added before loading the weather UI

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,10 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:clima/services/networking.dart';
+import 'location_screen.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -31,20 +33,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
         url:
             "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey");
     var weatherData = await networkHelper.getData();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //getLocation();
-          },
-          child: Text('Get Location'),
-        ),
+        body: Center(
+      child: SpinKitFadingCircle(
+        color: Colors.white,
+        size: 100,
       ),
-    );
+    ));
   }
 
   @override
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 
 class LocationScreen extends StatefulWidget {
+  final String locationWeather;
+  LocationScreen({this.locationWeather});
   @override
   _LocationScreenState createState() => _LocationScreenState();
 }
